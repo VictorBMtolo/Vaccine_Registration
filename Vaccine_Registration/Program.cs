c24a4a5162ce77cfdaf893e6f8cca73980c102e3
VictorBMtolo <Victormtolo6@gmail.com>
2021-08-18 10:42:06 +0200

    Changes to DOB

---
+++ Vaccine_Registration/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Vaccine_Registration
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ID: ");
+            string IDnum = Console.ReadLine();
+
+            Console.WriteLine("Surname: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Firstname: ");
+            string Firstname = Console.ReadLine();
+
+            Console.WriteLine("Gender: ");
+            string Gender = Console.ReadLine();
+
+            Console.WriteLine("Date of Registration: ");
+            string DOB = Console.ReadLine();
+
+            Console.WriteLine("Province: ");
+            string Province = Console.ReadLine();
+
+        }
+    }
+}
